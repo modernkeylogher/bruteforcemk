bd03965af4122ec784e34e61913f1004c8d2a06d
muhil <63582922+modernkeylogher@users.noreply.github.com>
2020-04-14 14:47:19 +0530

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,9 @@
+pkg install figlet -y
+pkg install toilet -y
+pkg install ruby -y
+pkg install python
+pkg install python2
+pkg install nano
+pkg install toilet
+gem install lolcat
+pip install requests
